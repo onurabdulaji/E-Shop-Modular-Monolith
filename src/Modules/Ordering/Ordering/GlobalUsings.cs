c963110131b21f6a69cf3915773e2e879a3e9381
onurabdulaji <onurabdulaji@gmail.com>
2025-04-26 13:30:35 +0200

    Casess

--- src/Modules/Ordering/Ordering/GlobalUsings.cs
+++ src/Modules/Ordering/Ordering/GlobalUsings.cs
@@ -1,6 +1,9 @@
 ﻿global using MediatR;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.Extensions.Logging;
 global using Ordering.Orders.Events;
 global using Ordering.Orders.Models;
 global using Ordering.Orders.ValueObjects;
 global using Shared.DDD;
+global using System.Reflection;
