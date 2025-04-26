2ddbe044c1468814662dfeaab854582cfa94b3db
onurabdulaji <onurabdulaji@gmail.com>
2025-04-26 10:14:49 +0200

    IntegrationEvent added,
    ProductPrice aded

---
+++ src/Shared/Shared.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Messaging.Events;
+public record IntegrationEvent
+{
+    public Guid EventId => Guid.NewGuid();
+    public DateTime OccuredOn => DateTime.UtcNow;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
