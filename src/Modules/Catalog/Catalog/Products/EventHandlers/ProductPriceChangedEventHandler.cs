fce1b3378758354019f4059e8f69cdbba655bcf6
onurabdulaji <onurabdulaji@gmail.com>
2025-04-26 10:30:45 +0200

    ProductPriceChangedEventHandler modifed

--- src/Modules/Catalog/Catalog/Products/EventHandlers/ProductPriceChangedEventHandler.cs
+++ src/Modules/Catalog/Catalog/Products/EventHandlers/ProductPriceChangedEventHandler.cs
@@ -1,11 +1,25 @@
-﻿namespace Catalog.Products.EventHandlers;
-public class ProductPriceChangedEventHandler(ILogger<ProductPriceChangedEventHandler> logger)
+﻿
+
+namespace Catalog.Products.EventHandlers;
+public class ProductPriceChangedEventHandler
+    (ILogger<ProductPriceChangedEventHandler> logger, IBus bus)
     : INotificationHandler<ProductPriceChangedEvent>
 {
-    public Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
+    public async Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
     {
-        // TODO publish integration to basket prices
         logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
-        return Task.CompletedTask;
+
+        var intgrationEvent = new ProductPriceChangedIntegrationEvent
+        {
+            ProductId = notification.Product.Id,
+            Name = notification.Product.Name,
+            Category = notification.Product.Category,
+            Description = notification.Product.Description,
+            ImageFile = notification.Product.ImageFile,
+            Price = notification.Product.Price, // set updated prodcut price
+        };
+
+        await bus.Publish(intgrationEvent, cancellationToken);
+
     }
 }
