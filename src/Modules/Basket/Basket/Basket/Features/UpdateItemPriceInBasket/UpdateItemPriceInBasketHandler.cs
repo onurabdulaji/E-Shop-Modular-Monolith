d1609ed9da3f902993d7cf9956f203a6c1bf31fb
onurabdulaji <onurabdulaji@gmail.com>
2025-04-26 10:50:05 +0200

    New Feature

--- src/Modules/Basket/Basket/Basket/Features/UpdateItemPriceInBasket/UpdateItemPriceInBasketHandler.cs
+++ src/Modules/Basket/Basket/Basket/Features/UpdateItemPriceInBasket/UpdateItemPriceInBasketHandler.cs
@@ -14,16 +14,29 @@ public class UpdateItemPriceInBasketCommandValidator : AbstractValidator<UpdateI
 }
 
 internal class UpdateItemPriceInBasketHandler
-    ()
+    (BasketDbContext dbContext)
     : ICommandHandler<UpdateItemPriceInBasketCommand, UpdateItemPriceInBasketResult>
 {
-    public Task<UpdateItemPriceInBasketResult> Handle(UpdateItemPriceInBasketCommand command, CancellationToken cancellationToken)
+    public async Task<UpdateItemPriceInBasketResult> Handle(UpdateItemPriceInBasketCommand command, CancellationToken cancellationToken)
     {
         // find shopping cart items with a productId 
         // Iterate items and Update price every time
         // save to db 
         // return result
 
-        throw new NotImplementedException();
+        var itemsToUpdate = await dbContext.ShoppingCartItems
+            .Where(q => q.ProductId == command.ProductId)
+            .ToListAsync(cancellationToken);
+
+        if (!itemsToUpdate.Any()) return new UpdateItemPriceInBasketResult(false);
+
+        foreach (var item in itemsToUpdate)
+        {
+            item.UpdatePrice(command.Price);
+        }
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new UpdateItemPriceInBasketResult(true);
     }
 }
